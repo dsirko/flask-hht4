cef671ce18b7a942b7ada7c1a5a2b3494ea4e1e4
Dmytro Sirko <dimasirko@gmail.com>
2024-02-10 12:38:50 +0200

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World2!'
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return 'Contact page'
 
 
 if __name__ == '__main__':
